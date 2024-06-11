52634f8eca03f535fe2af2227cb199c980a1a1bf
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-06-12 01:38:21 +0900

    feat: try setting up shuttle shared db

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use axum::{routing::get, Router};
+use shuttle_runtime::CustomError;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_shared_db::Postgres] pool: sqlx::PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(CustomError::new)?;
+
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
